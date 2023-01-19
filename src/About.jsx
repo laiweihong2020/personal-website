8665fe66655a2a3ce5552984b25d2bfda2f82a6e
NotAHorse <weihonglai20@gmail.com>
2023-01-19 21:38:09 +0800

    Added react to github pages

---
+++ src/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>About Page</h2>
+                <main>
+                    <p>This is the About page</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default About;
